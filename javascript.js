30c799f3d5754d83c5375135cc44db2b491fdcd5
Caleb Canada <calebcanada13@gmail.com>
2018-08-13 11:07:30 -0500

    check in

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
 document.getElementById("Headline").innerText="Practice,Practice";
 
+var addName = document.getElementById("genericId").innerHTML;
+document.getElementById("genericId").innerHTML=addName + ' Caleb C';
 
 document.getElementById("kennButton").innerText="Move on";
